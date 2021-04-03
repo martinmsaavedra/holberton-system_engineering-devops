077bb84c5dee9760179e05a531cbc6318715fdf9
martinmsaavedra <martin@bildung.uy>
2021-04-03 19:08:35 +0000

    fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
 # Changes to our configuration file
 
 file_line {'No_password':
-    ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => '    PasswordAuthentication no',
-    match  => 'PasswordAuthentication yes',
+    match  =>  'PasswordAuthentication yes',
 }
 file_line {'Key_file':
-    ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => '    IdentityFile ~/.ssh/holberton',
 }
