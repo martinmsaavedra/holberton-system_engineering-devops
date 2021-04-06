3ab48c04a7645c3ac009bbd72efdbba194354727
martinmsaavedra <martin@bildung.uy>
2021-04-06 18:06:10 +0000

    task 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,21 @@
 #  Install Nginx web serve
 package { 'nginx':
-  provider => 'apt-get',
-  ensure   => 'present',
+  ensure   => present,
   name     => 'nginx',
 }
 
 file { 'index.html':
-  ensure  => 'present',
+  ensure  => present,
   path    => '/etc/nginx/html/index.html',
   content => 'Holberton School',
 }
 
 file_line { 'setup':
+  ensure => present,
   path   => '/etc/nginx/sites-available/default',
-  ensure => 'present',
   after  => '[::80]:80 default_server;'
-  line   => 'rewrite ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent';,
-
+  line   => 'rewrite ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
 
 service {'nginx':
   ensure  => running,
