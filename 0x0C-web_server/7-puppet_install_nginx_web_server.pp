17de6f1cbba4bbd26a19c8c4d9245c4d58f139d2
martinmsaavedra <martin@bildung.uy>
2021-04-06 18:16:23 +0000

    fix 6

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,23 +1,27 @@
-#  Install Nginx web serve
-package { 'nginx':
-  ensure   => present,
-  name     => 'nginx',
-}
+#Install and configure nginx server
 
-file { 'index.html':
-  ensure  => present,
-  path    => '/etc/nginx/html/index.html',
-  content => 'Holberton School',
-}
-
-file_line { 'setup':
+package {'nginx':
   ensure => present,
+  name   => 'nginx',
+}
+
+file {'index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School\n',
+}
+
+file_line { 'redirect_me':
+  ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => '[::80]:80 default_server;'
+  after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-service {'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['redirect_me'],
 }
