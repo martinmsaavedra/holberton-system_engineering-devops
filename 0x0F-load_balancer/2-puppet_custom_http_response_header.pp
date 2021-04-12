62769a30df9790d258585dbcb63e0001642daac8
martinmsaavedra <martin@bildung.uy>
2021-04-12 17:42:40 +0000

    task 1

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,37 @@
-node 'haproxy-server' {
-  include ::haproxy
-  haproxy::listen { 'puppet00':
-    collect_exported => false,
-    ipaddress        => $::ipaddress,
-    ports            => '80',
-    balance          => 'roundrobin',
-  }
+#Install and configure nginx server
 
+package {'nginx':
+  ensure => present,
+  name   => 'nginx',
+}
+
+package {'haproxy':
+  ensure => present,
+  name   => 'haproxy',
+}
+
+file {'index.html':
+  ensure  => present,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School\n',
+}
+
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['redirect_me'],
+}
+
+service { 'haproxy':
+  ensure     => running,
+  enable     => true,
 }
