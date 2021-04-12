fb049744dc66c75cd57f3174c6df99bedb8be56d
martinmsaavedra <martin@bildung.uy>
2021-04-12 17:33:09 +0000

    task 1

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,10 @@
-ss
+node 'haproxy-server' {
+  include ::haproxy
+  haproxy::listen { 'puppet00':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '80',
+    balance          => 'roundrobin',
+  }
+
+}
