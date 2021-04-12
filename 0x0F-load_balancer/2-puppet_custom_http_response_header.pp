9f8452efc89a30edb191d3df858bac59341729ef
martinmsaavedra <martin@bildung.uy>
2021-04-12 17:43:38 +0000

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,37 +1,35 @@
-#Install and configure nginx server
-
-package {'nginx':
-  ensure => present,
-  name   => 'nginx',
+# Puppet manifest to create server and add custom header
+exec { 'update':
+  command => '/usr/bin/apt-get update',
 }
 
-package {'haproxy':
-  ensure => present,
-  name   => 'haproxy',
+package { 'nginx':
+  ensure => installed,
 }
 
-file {'index.html':
-  ensure  => present,
-  path    => '/var/www/html/index.html',
-  content => 'Holberton School\n',
+file_line { 'redirect_line':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+file_line { 'new_header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  require    => Package['nginx'],
-  subscribe  => File_line['redirect_me'],
-}
-
-service { 'haproxy':
-  ensure     => running,
-  enable     => true,
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
 }
