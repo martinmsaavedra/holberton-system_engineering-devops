a5d0ee7ac4f2abfa2e74ee0a77fb8387542395bc
martin saavedra <martin@bildung.uy>
2021-05-18 15:47:07 +0000

    task 0-1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests as r
 def number_of_subscribers(subreddit):
     '''Returns the number of subscribers'''
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    x = r.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers)
+    x = r.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers, allow_redirects=False)
     if x.status_code == 404:
         return 0
     response = x.json().get('data')
